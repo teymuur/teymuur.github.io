5c54afbfc45d309df74665db4d3e2d4e35cb011c
teymuur <teymur_babayev08@yahoo.com>
2024-07-03 16:55:38 +0400

    Updated

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ codevisible = false
     }
 
     });
+
+    function copytoclipboard() {
+        // Get the text field
+        var copyText = document.getElementById("code");
+      
+        // Select the text field
+        copyText.select();
+        copyText.setSelectionRange(0, 99999); // For mobile devices
+      
+         // Copy the text inside the text field
+        navigator.clipboard.writeText(copyText.value);
+      
+        // Alert the copied text
+        alert("Copied the text: " + copyText.value);
+      }
