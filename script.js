bec99ef3296187a40f9d6bd95955132a020d9904
teymuur <teymur_babayev08@yahoo.com>
2024-07-03 16:27:05 +0400

    Added script

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+
+    document.getElementById("install").addEventListener('click', function(event) {
+        event.preventDefault();
+        console.log(1); 
+        document.getElementById("code").style = "display: default";
+
+    });
