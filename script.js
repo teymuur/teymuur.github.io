f01fd68499feae7a46d4cd173b63c99b0d303778
teymuur <teymur_babayev08@yahoo.com>
2024-07-03 16:36:41 +0400

    Update

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 
-
-
-    document.getElementById("install").addEventListener('click', function(event) {
+installbtn =  document.getElementById("install")
+codevisible = false
+   installbtn.addEventListener('click', function(event) {
+    if(!codevisible){
         event.preventDefault();
-        console.log(1); 
+        
         document.getElementById("code").style = "display: default";
+        codevisible = true;
+    }else{
+        document.getElementById("code").style = "display: none";
+        codevisible = false;
+    }
 
     });
