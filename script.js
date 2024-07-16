4f65425f67b5985538c29d1c1012b94083bf6c75
teymuur <teymur_babayev08@yahoo.com>
2024-07-17 00:08:56 +0400

    Updated

--- script.js
+++ script.js
@@ -85,3 +85,65 @@ codevisible = false
 
     // Fetch and display random images from the 'EarthPorn' subreddit
     fetchRandomImages('memes', 8);
+
+    document.addEventListener("DOMContentLoaded", function() {
+        const ctx = document.getElementById('skillsChart').getContext('2d');
+        const skillsData = {
+            labels: ['Python', 'PHP', 'JavaScript', "Rust",'C#', 'C/C++', 'HTML', 'CSS', 'SQL', 'Database Adminstration','Git'],
+            datasets: [{
+                label: 'Skill Level',
+                data: [5, 4.5, 4,2, 3, 2, 4.5, 3, 4, 4,3],
+                backgroundColor: [
+                    'rgba(75, 192, 192, 0.4)',
+                    'rgba(75, 192, 100, 0.4)',
+                    'rgba(175, 16, 192, 0.4)',
+                    'rgba(153, 102, 255, 0.4)',
+                    'rgba(255, 159, 64, 0.4)',
+                    'rgba(275, 255, 2, 0.4)',
+                    'rgba(255, 25, 25, 0.4)',
+                    'rgba(75, 252, 75, 0.4)',
+                    'rgba(180, 192, 190, 0.4)',
+                    'rgba(15, 16, 190, 0.4)',
+                    'rgba(193, 192, 5, 0.4)',
+    
+                    
+                ],
+                borderColor: [
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(75, 192, 100, 1)',
+                    'rgba(175, 16, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(275, 255, 2, 1)',
+                    'rgba(255, 25, 25, 1)',
+
+                ],
+                borderWidth: 1
+            }]
+        };
+    
+       
+        const config = {
+            type: 'bar',
+            data: skillsData,
+            options: {
+                plugins: {
+                    legend: {
+                        display: false  // Hide the legend
+                    }
+                },
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        max: 5,
+                        display: false  // Hide y-axis numbers
+                    }
+                },
+                
+            }
+        };
+
+    
+        const skillsChart = new Chart(ctx, config);
+    });
+    
